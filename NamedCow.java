98a0e355bfda28d82d8ca3305835649ff00d56b0
luaung <lumasudathaya@s.sfusd.edu>
2018-10-23 13:38:57 -0700

    ohhh

--- NamedCow.java
+++ NamedCow.java
@@ -1,4 +1,12 @@
-class NamedCow
+class NamedCow extends Cow
 {
+	NamedCow(String type,String name, String sound){
+		myType = type;
+		myName = name;
+		mySound = sound;
+	}
 
+	public void getName(){
+		return myName;
+	}
 }
